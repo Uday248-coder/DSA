14fb9b598b3561ce76f1f6b1dc33476801cda52e
Nextron <udaybansal4106@gmail.com>
2025-09-25 11:17:13 +0530

    Update Stacks.c

--- Data_Structs/Stacks.c
+++ Data_Structs/Stacks.c
@@ -1,1 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define MAX 1000
+typedef struct {
+    int stack[MAX];
+    int top;
+}S;
 
+void push(S* a, int val){
+    if(a->top ==MAX-1){
+        printf("OVERFLOW!");
+    }
+    else{
+        a->stack[++(a->top)]=val;
+    }
+}
+
+int pop(S* a){
+    if(a->top ==-1){
+        printf("UNDERFLOW!");
+        return -99999;
+    }
+    else{
+        return a->stack[(a->top)--];
+    }
+}
+int peek(S* a){
+    if(a->top ==-1){
+        printf("EMPTY!");
+        return -99999;
+    }
+    else{
+        return a->stack[a->top];
+    }
+}
