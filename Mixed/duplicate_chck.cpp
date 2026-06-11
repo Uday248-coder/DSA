d3d0820dd8cb2ef565d72ef5301ffc61ad41564f
Nextron <udaybansal4106@gmail.com>
2026-06-11 10:08:34 +0530

    Fix frequency counting in containsDuplicate method
    Corrected frequency counting logic in containsDuplicate function.

--- Mixed/duplicate_chck.cpp
+++ Mixed/duplicate_chck.cpp
@@ -1,11 +1,13 @@
 //https://leetcode.com/problems/contains-duplicate/
+// corrected my freq approach, its correct1
+// but i believe not the optimal one...
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> freq;
         for(int i=0;i<nums.size();i++){
-            freq[i]++;
-            if(freq[i]>1)
+            freq[nums[i]]++;
+            if(freq[nums[i]]>1)
                 return true;
         }
         return false;
