8ab95305afce8524fe47776fcc73f95a1c984d39
Nextron <udaybansal4106@gmail.com>
2026-06-12 12:36:42 +0530

    Rename sort_colors.cpp to sort_colors___dutch_natnl_flag_alg.cpp

--- Arrays/sort_colors___dutch_natnl_flag_alg.cpp
+++ Arrays/sort_colors___dutch_natnl_flag_alg.cpp
@@ -34,3 +34,28 @@ class Solution {
         //     cout << i << " - - -" << endl;
     }
 };
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //dutch national flag algorithm applied
+        int l=0, m=0, h=nums.size()-1;
+        while(m<=h){
+            if(nums[m]==0){
+                nums[m]=nums[l];
+                nums[l]=0;
+                m++;
+                l++;
+            }
+            else if(nums[m]==1){
+                m++;
+            }
+            else if(nums[m]==2){
+                nums[m]=nums[h];
+                nums[h]=2;
+                h--;
+            }
+        }
+    }
+};
