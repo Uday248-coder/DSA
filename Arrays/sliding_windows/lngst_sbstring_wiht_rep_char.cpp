31625263ecc6c595ca7ba771b917ca6dcd0f5e34
Nextron <udaybansal4106@gmail.com>
2026-06-13 13:38:55 +0530

    Fix sliding window logic in longest substring function
    Refactor lengthOfLongestSubstring function to correctly manage the sliding window and frequency map for characters.

--- Arrays/sliding_windows/lngst_sbstring_wiht_rep_char.cpp
+++ Arrays/sliding_windows/lngst_sbstring_wiht_rep_char.cpp
@@ -1,23 +1,22 @@
 //https://leetcode.com/problems/longest-substring-without-repeating-characters/
-// brute force approach, doesnt work yet
+// updated version, has to learnt again
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         // i think we'll start a window counter and increase the winodw and store the enteries in a freq order if the freq doesnt match then we resize the window
         int maxi=0;
         unordered_map<char,int> freq;
-        int l=0, r=0;
+        int l=0,r=0;
         while(r<s.size()){
-            freq[s[r]]++;
-            if(freq.size() != r-l+1){
-                maxi = max(maxi,freq.size());
-                freq[s[l]]--;
-                if(freq[s[l]]==0)
-                    freq.erase(s[l]);
-                l++;
-            }
+            if(freq.contains(s[r]) && freq[s[r]] >= l)
+                l= freq[s[r]] + 1;    
+            freq[s[r]] = r;
+            maxi = max(maxi, r-l+1);
             r++;
         }
+
+
         return maxi; 
     }
 };
