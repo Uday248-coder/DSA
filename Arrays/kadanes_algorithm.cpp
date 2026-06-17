32a65125908d7834354b18f0528b13dc67372fd5
Nextron <udaybansal4106@gmail.com>
2026-06-17 17:50:29 +0530

    Create kadanes_algorithm.cpp

---
+++ Arrays/kadanes_algorithm.cpp
@@ -0,0 +1,17 @@
+//https://www.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) {
+        // Code here
+        int maxi=arr[0];
+        int sum=0;
+        for(int i=0;i<arr.size();i++){
+            sum+=arr[i];
+            maxi = max(maxi,sum);
+            if(sum<0)
+                sum=0;
+        }
+        return maxi;
+    }
+};
