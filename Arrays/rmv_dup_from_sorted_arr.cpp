04412a33c124f652d3757f0f3e458d3689dcca0b
Nextron <udaybansal4106@gmail.com>
2026-06-18 16:35:23 +0530

    Optimize removeDuplicates with two-pointer technique
    Refactored removeDuplicates function to use two-pointer approach for better efficiency.

--- Arrays/rmv_dup_from_sorted_arr.cpp
+++ Arrays/rmv_dup_from_sorted_arr.cpp
@@ -1,20 +1,17 @@
 //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-// this is raw approach.
+// time complexity -- O(n)
+// space somplexity -- O(1)
 class Solution {
 public:
-    // time complexity -- O(n)
-    // space somplexity -- O(n)
     int removeDuplicates(vector<int>& nums) {
-        // i can work around with hash maps right? jjust giving a try
-        unordered_map<int, int> map;
-        for(int i:nums)
-            map[i]=i;
-        int k=0;
-        nums.clear();
-        for(auto i:map)
-            nums.push_back(i.first);
-        sort(nums.begin(), nums.end());
-        return map.size();
+        // i think i can make the two pointer approach by myself.
+        int l=0;// it will store the pointer for the last distinct element found
+        for(int i=1;i<nums.size();i++){ // our second pointer that shall iterate over array
+            if(nums[i]!=nums[l]){ // if the iterator pointer finds a new element that is so far not discovered(since arr is sorted)
+                nums[++l]=nums[i]; // we just overwrite the position and update the lastpointer to point to this newly discovered element
+            }
+        }
+        return l+1; // we return the size by index + 1
     }
 };
