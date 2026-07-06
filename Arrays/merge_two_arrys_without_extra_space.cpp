6bc90b370862e3fffdc99ee8478d511865e07880
Nextron <udaybansal4106@gmail.com>
2026-07-06 22:50:34 +0530

    corrected the approach by using gap method taken from shell sort

--- Arrays/merge_two_arrys_without_extra_space.cpp
+++ Arrays/merge_two_arrys_without_extra_space.cpp
@@ -5,33 +5,63 @@
 
 // given two sorted arrays a and b where a should hold the first sorted elements from a+b and b to hold the later half.. i'll use two pointer method for checking if the element from b is present in a or if its the largest.. kinda thign/
 
-
 class Solution {
-  public:
-    void mergeArrays(vector<int>& a, vector<int>& b) {
-        // code here
-        int pa=0, pb=0; // pointer of a and b respectively
-        while(pb<b.size()){
-            if(a[pa]>b[pb]){
-                a[pa]=a[pa]^b[pb];
-                b[pb]=a[pa]^b[pb];
-                a[pa]=a[pa]^b[pb];
-                // pb=(pb+1<b.size())?pb+1:pb;
-            }
-            pa++;
-            if(pa>a.size()){
-                pb++;
-                pa=0;
-            }
-        }
-        for(pb=0;pb<b.size()-1;pb++){
-            for(pa=pb+1;pa<b.size();pa++){
-                if(b[pa]<=b[pb]){
-                    b[pa]=b[pa]^b[pb];
-                    b[pb]=b[pa]^b[pb];
-                    b[pa]=b[pa]^b[pb];
-                }
-            }
-        }
-    }
+	public:
+	void mergeArrays(vector<int>& a, vector<int>& b) {
+		// have to use gap method from shell sort
+		
+		int gap = ((b.size() + a.size())/2) + (b.size() + a.size())%2 ;
+		while (gap>0) {
+			int l = 0, r = gap ;
+			bool flip_l = false; // will be used to switch the pointer use of l from a to b..
+			bool flip_r = false;
+			if (r >= a.size()) {
+				r -= a.size();
+				flip_r = true ; // r will point inside the second array.
+			}
+// 			cout << "  GAP  " << gap << endl;
+			while (true) {
+			    if(flip_r && r>=b.size())
+				    break;
+				if (flip_l && flip_r) { // both true :: l and r both point inside array b
+					if (b[l]>b[r])
+						swap(b[l], b[r]);
+					l++;
+					r++;
+				}
+				else if (!flip_l && flip_r) // l points to 'a' while r points inside 'b'
+					{
+					if (a[l]>b[r])
+						swap(a[l], b[r]);
+					l++;
+					r++;
+					if (l >= a.size()) {
+						l -= a.size();
+						flip_l = true;
+					}
+				}
+				else if (!flip_l && !flip_r) { // both point inside 'a'
+					if (a[l]>a[r])
+						swap(a[l], a[r]);
+					l++;
+					r++;
+					if (l >= a.size()) {
+						l -= a.size();
+						flip_l = true;
+					}
+					if (r >= a.size()) {
+						r -= a.size();
+						flip_r = true;
+					}
+				}
+				
+				
+			}
+			if(gap==1)
+			    break;
+			gap = gap/2 + gap%2;
+		}
+	}
+};
+
 };
