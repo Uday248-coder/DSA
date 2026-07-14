ecd8a573eeeb218d1369d5312632623981aa82fa
Nextron <udaybansal4106@gmail.com>
2026-07-14 07:02:59 +0530

    brute force applied and worked. optimal to be found

---
+++ Arrays/cmmn_in_3_arrs.cpp
@@ -0,0 +1,32 @@
+// https://www.geeksforgeeks.org/problems/common-elements1132/1
+// EXPECTED TIME COMPLEXITY --> O(N)
+// EXPECTED SPACE COMPLEXITY --> O(1)
+
+// CURRENT TIME COMPLEXITY --> O(NLOGN)
+// CURRENT SPACE COMPLEXITY --> O(N)
+
+
+
+// brute force approach -- used a freq counting map to check and match the common elements freq. 
+// method passed all test cases but an optimal solution of pointer methods might exist.
+class Solution {
+  public:
+    vector<int> commonElements(vector<int> &a, vector<int> &b, vector<int> &c) {
+        //using auxilary space
+        unordered_map<int, int> map; // O(N) SPACE
+        for(int i:a) // O(N) TIME
+            map[i]=1;
+        for(int i:b)
+            map[i] = (map[i]==1)?map[i]+1:map[i];
+        for(int i:c)
+            map[i] = (map[i]==2)?map[i]+1:map[i];
+        
+        vector<int> ans; //O(N) SPACE
+        for(auto& [key,value] : map){ // O(N) TIME
+            if(value==3)
+                ans.push_back(key);
+        }
+        sort(ans.begin(), ans.end()); // O(NLOGN) TIME + O(N) SPACE
+        return ans;
+    }
+};
