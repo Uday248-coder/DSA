4bda0b1a39588e68a355bc827da3f572590ad6b2
Nextron <udaybansal4106@gmail.com>
2026-07-16 10:18:56 +0530

    Added optimal solution

--- Arrays/cmmn_in_3_arrs.cpp
+++ Arrays/cmmn_in_3_arrs.cpp
@@ -30,3 +30,36 @@ class Solution {
         return ans;
     }
 };
+
+/// optimal solution
+// CURRENT TIME COMPLEXITY --> O(N)
+// CURRENT SPACE COMPLEXITY --> O(1)
+class Solution {
+  public:
+    vector<int> commonElements(vector<int> &a, vector<int> &b, vector<int> &c) {
+        // three pointer approach
+        int i=0, j=0,k=0;
+        vector<int> ans;
+        while(i<a.size() && j<b.size() && k<c.size()){
+            if(a[i]==b[j] && b[j]==c[k]){ // for duplicate avoidance
+                if(ans.empty() || a[i]!= ans.back()){
+                    ans.push_back(a[i]);
+                }
+                i++;
+                j++;
+                k++;
+            }
+            else{
+                if(a[i]<b[j])
+                    i++;
+                else if(b[j]<c[k])
+                    j++;
+                else
+                    k++;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
