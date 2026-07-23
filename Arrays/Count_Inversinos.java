9f7e927fd8e4530fe5419c8fe791e2a8a766bfe0
Nextron <udaybansal4106@gmail.com>
2026-07-23 10:32:46 +0530

    Implement inversion count using merge sort

--- Arrays/Count_Inversinos.java
+++ Arrays/Count_Inversinos.java
@@ -2,17 +2,49 @@
 
 // EXPECTED TIME COMPLEXITIES : O(N LOGN)
 // EXPECTED SPACE COMPLEXITIES : O(N)
+// we'll apply merge sort and manually increment on counts.
 
-// burte-force -> just linear checks -> TIME: O(N^2)
 class Solution {
-    public int inversionCount(int arr[]) {
-        int count=0;
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j])
-                    count++;
-            }
-        }
-        return count;
-    }
+	
+	int count = 0;
+	void mergeSort(int[] arr, int low, int high) {
+		if (low >= high)
+			return;
+		int mid = low + (high - low)/2 ;
+		mergeSort(arr, low, mid);
+		mergeSort(arr, mid + 1, high);
+		merge(arr, low, mid, high);
+	}
+	
+	void merge(int[] arr, int low, int mid, int high) {
+		int[] temp = new int[high - low + 1];
+		int l = low;
+		int r = mid + 1;
+		int k = 0;
+		while (l <= mid && r <= high) {
+			if (arr[l] <= arr[r]) {
+				temp[k++] = arr[l];
+				l++;
+			} else {
+				temp[k++] = arr[r];
+				count += (mid - l + 1);
+				r++;
+			}
+		}
+		while (l <= mid) {
+			temp[k++] = arr[l++];
+		}
+		while (r <= high) {
+			temp[k++] = arr[r++];
+		}
+		for (int i = low; i<=high; i++) {
+			arr[i] = temp[i - low];
+		}
+	}
+	
+	public int inversionCount(int arr[]) {
+	    count=0;
+		mergeSort(arr,0,arr.length-1);
+		return count;
+	}
 }
