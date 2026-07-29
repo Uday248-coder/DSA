465260a3250166891a5612ef91749baa1f2761b4
Nextron <udaybansal4106@gmail.com>
2026-07-29 19:00:04 +0530

    Add two-pointer method to move negative elements
    Implemented a two-pointer approach to move negative elements to the end of the array without using extra space.

--- Arrays/mov_ngtv_ele_to_end.cpp
+++ Arrays/mov_ngtv_ele_to_end.cpp
@@ -1,4 +1,5 @@
 //https://www.geeksforgeeks.org/problems/move-all-negative-elements-to-end1813/1
+// this approach uses extra space.. if we have to segregate positive's and negative's in constant space then a new approach is needed right.
 class Solution {
   public:
     void segregateElements(vector<int>& arr) {
@@ -28,3 +29,25 @@ class Solution {
             arr.push_back(i);
     }
 };
+
+
+// the constant space solution :: using two pointer partition approach.
+
+vector<int> move(vector<int> &arr) {
+  int l=0, r=arr.size()-1;
+  while(l < r){
+    while(l<r && arr[l]<0){ // ignore the negatives that are in extreme left (in their respective ends)
+      l++;
+    }
+    // now l points to the leftmost positive element;
+    while(r>l && arr[r]>0){ // sameways move past all the positive elements as they are in their respective corner
+      r--;
+    } //  r points to the rightmost negative number
+    if(l<r){ // if still left is less than right, that means they are not in their correct position, hence swap them.
+      swap(arr[l],arr[r]);
+      l++;
+      r--;
+    }
+  }
+  return arr;
+}
