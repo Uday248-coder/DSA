f8bb00586b9ea21a2c35dc89ba22ffc18a5928ca
Nextron <udaybansal4106@gmail.com>
2026-08-01 10:38:00 +0530

    optimal.

--- Arrays/buy_and_sell_stocks_II.cpp
+++ Arrays/buy_and_sell_stocks_II.cpp
@@ -1,37 +1,20 @@
-//
+//https://www.geeksforgeeks.org/problems/buy-and-sell-a-share-at-most-twice/1
 // TIME : O(N)
 // SPACE : O(N)
 
-// Trash Approach. fails. bad failure.
 class Solution {
-  public:
-    vector<int> transaction(vector<int> arr,int start, int end){
-        vector<int> indexes(2,start);
-        int mini=start;
-        int maxi_profit=0;
-        for(int i=start;i<=end;i++){
-            if(arr[i]<arr[mini]){
-                mini=i;
-            }
-            if(maxi_profit < arr[i] - arr[mini]){
-                maxi_profit = arr[i] - arr[mini];
-                indexes[0] = mini;
-                indexes[1] = i;
-            }
-        }
-        return indexes;
-    }
-  
-    int maxProfit(vector<int> &prices) {
-        vector<int> first = transaction(prices, 0, prices.size()-1);
-        int ans = prices[first[1]] - prices[first[0]];
-        first = transaction(prices,0,first[0]-1);
-        int temp = prices[first[1]] - prices[first[0]];
-        
-        first = transaction(prices,first[1]+1, prices.size()-1);
-        int temp2 = prices[first[1]] - prices[first[0]];
-        
-        ans += max(temp, temp2);
-        return ans;
-    }
+	public:
+	int maxProfit(vector<int> &prices) {
+		int buy1 = INT_MIN, sell1 = 0;
+		int buy2 = INT_MIN, sell2 = 0;
+		
+		for (int p : prices) {
+			buy1 = max(buy1, -p);
+			sell1 = max(sell1, buy1 + p);
+			buy2 = max(buy2, sell1 - p);
+			sell2 = max(sell2, buy2 + p);
+		}
+		
+		return sell2;
+	}
 };
