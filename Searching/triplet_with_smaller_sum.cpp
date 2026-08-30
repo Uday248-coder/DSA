4a5978300159943c7b6c235dd141ac48fcb2c6c3
Nextron <udaybansal4106@gmail.com>
2026-08-30 10:37:15 +0530

    Optimize countTriplets using sorting and two-pointer
    Refactored countTriplets function to use sorting and a two-pointer approach for better performance. Removed brute-force implementation and improved logic for counting triplets.

--- Searching/triplet_with_smaller_sum.cpp
+++ Searching/triplet_with_smaller_sum.cpp
@@ -3,44 +3,22 @@
 // TC : O(N^2)
 // SC : O(1)
 
-// brute force --  tried 2-pointer over a fixed .. 
+// we could have sorted the array for the best two-pointer count stratergy.. hence used it.. 
 class Solution {
   public:
     int countTriplets(int sum, vector<int>& arr) {
+        sort(arr.begin(), arr.end());
         int count = 0;
-        for(int i=0;i<arr.size();i++){
-            int l=i+1;
-            int r=arr.size()-1;
+        for(int i=0;i<arr.size();i++)
+        {
+            int l=i+1, r=arr.size()-1;
             while(l<r){
-                int curr= arr[i]+arr[l]+arr[r];
-                if(curr < sum){
-                    // cout <<  arr[i] << " " << arr[l] <<" "<<arr[r] << endl;
-                    count++;
-                    // l++;
-                    r--;
-                }else if(curr > sum){
-                    r--;
-                }else{
+                if(arr[i]+arr[l]+arr[r] < sum){
+                    count += (r-l);
                     l++;
                 }
-            }
-        }
-        return count;
-    }
-};
-
-// brute-force 2 || didnt work for test cases.. can't identify the problem yet..
-class Solution {
-  public:
-    int countTriplets(int sum, vector<int>& arr) {
-        int count =0;
-        for(int i=0;i<arr.size();i++){
-            for(int j=i+1;j<arr.size();j++){
-                int x=j+1;
-                while(arr[i]+arr[j]+arr[x] < sum && x<arr.size()){
-                    // cout << arr[i] << " " << arr[j] << " " <<  arr[x] << endl;
-                    count++;
-                    x++;
+                else{
+                    r--;
                 }
             }
         }
