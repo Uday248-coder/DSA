8396797b33a4e8d06fa9507caa2fdd5cc184c065
Nextron <udaybansal4106@gmail.com>
2026-09-18 12:32:36 +0530

    Optimize Bishu and Soldiers algorithm
    Optimized the algorithm to O(N log N) by implementing binary search and using prefix sums for cumulative power calculations.

--- Searching/BIshu_and_Soldier.cpp
+++ Searching/BIshu_and_Soldier.cpp
@@ -1,55 +1,55 @@
 //https://www.hackerearth.com/problem/algorithm/bishu-and-soldiers-227/?utm_source=header&utm_medium=search&utm_campaign=he-search
-
-//brute force - O(N^2) time.. can be optimised further... i just sorted the powers of soldiers because it will help me in reducing my search space.. next optimisation planned is using binary search to find the mid point and use prefix sum to directly fetch the cummulative sum.
-/*
-// Sample code to perform I/O:
-
+// optimised version - O(NlogN) - used binary search per iteration and then used sorted array's property.
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-	int num;
-	cin >> num;										// Reading input from STDIN
-	cout << "Input number is " << num << endl;		// Writing output to STDOUT
-}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-*/
+    int N;
+    if (!(cin >> N) || N <= 0) return 0;
 
-// Write your code here
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-	int N;
-	cin >> N;
-	vector<int> powers(N);
-	for(int i=0;i<N;i++){
-		cin>>powers[i];
-	}
-	sort(powers.begin(), powers.end());
-	int Q;
-	cin>>Q;
-	vector<int> rounds(Q);
-	for(int i=0;i<Q;i++){
-		cin>>rounds[i];
-	}
-	vector<pair<int,int>> answer;
-	for(int i=0;i<Q;i++){
-		int sum=0;
-		int count=0;
-		for(int j=0;j<N;j++){
-			if(powers[j]<=rounds[i]){
-				sum+=powers[j];
-				count++;
-			}else{
-				break;
-			}
-		}
-		answer.push_back({count,sum});
-	}
-	for(pair<int,int> a: answer){
-		cout << a.first << " " << a.second << endl;
-	}
-	return 0;
+    vector<int> powers(N);
+    for (int i = 0; i < N; i++) {
+        cin >> powers[i];
+    }
+
+    sort(powers.begin(), powers.end());
+
+    vector<int> powcum(N);
+    powcum[0] = powers[0];
+    for (int i = 1; i < N; i++) {
+        powcum[i] = powcum[i - 1] + powers[i];
+    }
+
+    int Q;
+    cin >> Q;
+
+    while (Q--) {
+        int bishu_power;
+        cin >> bishu_power;
+
+        int count = 0;
+        int l = 0, h = N - 1;
+
+        
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (powers[m] <= bishu_power) {
+                count = m + 1; 
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+
+        int sum = (count > 0) ? powcum[count - 1] : 0;
+        cout << count << " " << sum << "\n";
+    }
+
+    return 0;
 }
